5599e9be14a131ec633c299d278185fbdac554f0
Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>
2013-02-17 11:10:40 -0600

    Nine-patch resizing test

--- AAR.py
+++ AAR.py
@@ -2,6 +2,14 @@
 
 from PIL import Image
 import os
-import Asset
+from Assets import Asset
 
+asset = Asset.open_asset("test.png", Asset.XHDPI)
+asset.downscale(Asset.HDPI).save("debug/out-png-hdpi.png")
+asset.downscale(Asset.MDPI).save("debug/out-png-mdpi.png")
+asset.downscale(Asset.LDPI).save("debug/out-png-ldpi.png")
 
+asset = Asset.open_asset("test.9.png", Asset.XHDPI)
+asset.downscale(Asset.HDPI).save("debug/out-png-hdpi.9.png")
+asset.downscale(Asset.MDPI).save("debug/out-png-mdpi.9.png")
+asset.downscale(Asset.LDPI).save("debug/out-png-ldpi.9.png")
